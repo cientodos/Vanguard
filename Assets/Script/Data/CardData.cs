5adb1dfd726823d28e0854adcbfeb4b3ba00f0c6
cientodos <139424990+cientodos@users.noreply.github.com>
2026-08-17 22:44:12 +0900

    구글스프레드시트 연동해야함

---
+++ Assets/Script/Data/CardData.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class CardData : MonoBehaviour
+{
+    public string id;
+    public string cardName;
+    public int grade;
+    public int power;
+    public int shield;
+    public int critical;
+    public string country;
+    public string tribe;
+    public string flavorText;
+    public string triggerType;
+    public string cardType;
+    public string availableRarities;
+    public string spriteAssetKeys;
+}
